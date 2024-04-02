547565987651f75bd5593283e6e894a250da94c1
pavlobarabah <pavlobarabah@gmail.com>
2024-04-02 13:07:24 +0300

    learned Bit Operations

--- src/main/java/org/example/lesson2/BitOperations.java
+++ src/main/java/org/example/lesson2/BitOperations.java
@@ -7,8 +7,8 @@ public class BitOperations {
 
         // Побітове І (AND)
         int andResult = a & b; // Результат: 2 (0010 в двійковій системі)
-        System.out.println(Integer.toBinaryString(10));
-        System.out.println(Integer.toBinaryString(6));
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
         System.out.println("a & b = " + andResult);
         System.out.println(Integer.toBinaryString(andResult));
 
@@ -16,13 +16,50 @@ public class BitOperations {
 
         // Побітове АБО (OR)
         int orResult = a | b;  // Результат: 14 (1110 в двійковій системі)
-        System.out.println(Integer.toBinaryString(10));
-        System.out.println(Integer.toBinaryString(6));
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
         System.out.println("a | b = " + orResult);
         System.out.println(Integer.toBinaryString(orResult));
 
         System.out.println("-----------------------------------------------------------------------------------");
 
+        // Побітове Виключне АБО (XOR)
+        int xorResult = a ^ b; // Результат: 12 (1100 в двійковій системі)
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println(Integer.toBinaryString(b));
+        System.out.println("a ^ b = " + xorResult);
+        System.out.println(Integer.toBinaryString(xorResult));
 
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        // Побітовий зсув вліво (Left Shift)
+        int leftShiftResult = a << 1; // Результат: 20 (10100 в двійковій системі)
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println("a << 1 = " + leftShiftResult);
+        System.out.println(Integer.toBinaryString(leftShiftResult));
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        // Побітовий зсув вправо (Right Shift)
+        int rightShiftResult = a >> 1; // Результат: 5 (0101 в двійковій системі)
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println("a >> 1 = " + rightShiftResult);
+        System.out.println(Integer.toBinaryString(rightShiftResult));
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        // Логічний побітовий зсув вправо з заповненням нулями (Zero-fill Right Shift)
+        int zeroFillRightShiftResult = a >>> 1; // Результат: 5 (0101 в двійковій системі)
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println("a >>> 1 = " + zeroFillRightShiftResult);
+        System.out.println(Integer.toBinaryString(zeroFillRightShiftResult));
+
+        System.out.println("-----------------------------------------------------------------------------------");
+
+        // Побітове заперечення (NOT)
+        int notResult = ~a; // Результат: -11 (11111111111111111111111111110101 в двійковій системі)
+        System.out.println(Integer.toBinaryString(a));
+        System.out.println("~a = " + notResult);
+        System.out.println(Integer.toBinaryString(zeroFillRightShiftResult));
     }
 }
